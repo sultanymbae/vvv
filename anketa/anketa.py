89d52482c09b81f86114d4803c422ce2f60935bc
Your Name <sultannurbekov12@gmail.com>
2023-06-03 15:30:57 +0600

    3

--- anketa/anketa.py
+++ anketa/anketa.py
@@ -1,21 +1,3 @@
-class Questionary:
-    name = input("Введите свое имя")
-    surname = input("Введите фамилию")
-    age = int(input("Введите свой возраст"))
-    weight = float(input("Введите свой вес"))
-    height = float(input("Введите свой рост"))
-    gender = input("Какой ваш пол? (М/Ж)")
-
-    bmi = weight / (height ** 2)
-
-    print("\nСпасибо за заполнение анкеты.")
-    print("Ваши данные:")
-    print("Имя:", name)
-    print("Возраст:", age)
-    print("Вес:", weight, "кг")
-    print("Рост:", height, "м")
-    print("Пол:", gender)
-    print("Индекс массы тела (ИМТ):", bmi)
 
 
 
